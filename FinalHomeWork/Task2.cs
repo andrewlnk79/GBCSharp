1d5c531aaf2b87b2227fd48f79129ce2d6349caf
Andrey Linnik <av.linnik79@ya.ru>
2024-03-03 10:07:34 +0300

    help commit

---
+++ FinalHomeWork/Task2.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalHomeWork
+{
+    public class Task2
+    {
+        public void AkkermanFunction(int num1, int num2)
+        {
+
+        }
+    }
+}
