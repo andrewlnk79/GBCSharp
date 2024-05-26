17cde8bd256ba47a330f08558c174ba321265c36
Linnik <av.linnik79@ya.ru>
2024-05-26 09:53:20 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace DiveCsharp1
+    {
+    internal class Program
+        {
+        static void Main(string[] args)
+            {
+            Console.WriteLine("Hello, World!");
+            }
+        }
+    }
