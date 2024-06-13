e0e2d65b84a6de6a8609e1c169df6a96f194f8ac
Andrey Linnik <92717307+andrewlnk79@users.noreply.github.com>
2024-06-13 15:03:16 +0300

    appHomeWork2

--- TypeConversion/Bits.cs
+++ TypeConversion/Bits.cs
@@ -1,6 +1,19 @@
 ﻿namespace TypeConversion;
 
 public class Bits
-{
-    
-}
+    {
+    public long value { get; private set; } = 0;
+
+    public Bits(long num)
+        {
+        this.value = num;
+        }
+
+    public static explicit operator long(Bits num) => (long)num.value;
+    public static implicit operator Bits(long num) => new Bits(num);
+    public static explicit operator int(Bits num) => (int)num.value;
+    public static implicit operator Bits(int b) => new Bits(b);
+
+    public static explicit operator byte(Bits num) => (byte)num.value;
+    public static implicit operator Bits(byte num) => new Bits(num);
+    }
